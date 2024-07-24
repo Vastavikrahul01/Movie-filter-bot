6f4c8e24e6aa377af4ae7dd35a126ab30de9be18
Vastavikrahul01 <165668531+Vastavikrahul01@users.noreply.github.com>
2024-07-24 22:00:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USERNAME = environ.get('USERNAME', "https://telegram.me/Vastavikrahul")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002163781954'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/VastavikMovieFlix')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001234567898').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rahul0304:rahul@cluster0.pqqgweh.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Vastavikrahul:bsbeKguz9QE9cQBr@cluster0.nmcak6n.mongodb.net/?retryWrites=true&w=majority")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
